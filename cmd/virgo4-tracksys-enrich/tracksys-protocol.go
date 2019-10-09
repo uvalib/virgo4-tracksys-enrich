942f420c8dcf1023e91a437036b9c1c15b3df2db
UVADave <dpg3k@virginia.edu>
2019-10-09 12:17:37 -0400

    Cleanup...

--- cmd/virgo4-tracksys-enrich/tracksys-protocol.go
+++ cmd/virgo4-tracksys-enrich/tracksys-protocol.go
@@ -7,9 +7,6 @@ import (
 	"net/http"
 	"strings"
 	"time"
-	//"regexp"
-	//"strconv"
-	//"github.com/antchfx/xmlquery"
 )
 
 var maxHttpRetries = 3
@@ -91,47 +88,6 @@ func (cl *cacheLoaderImpl) httpGet(url string) ([]byte, error) {
 	//log.Printf( body )
 	return body, nil
 }
-
-//func ( s * solrImpl ) processResponsePayload( body []byte ) ( int, uint, error ) {
-//
-//	// generate a query structure from the body
-//	doc, err := xmlquery.Parse( bytes.NewReader( body ) )
-//	if err != nil {
-//		return 0, 0, err
-//	}
-//
-//	// attempt to extract the statusNode field
-//	statusNode := xmlquery.FindOne( doc, "//response/lst[@name='responseHeader']/int[@name='status']")
-//	if statusNode == nil {
-//		return 0, 0, fmt.Errorf( "Cannot find status field in response payload (%s)", body )
-//	}
-//
-//	// if it appears that we have an error
-//	if statusNode.InnerText( ) != "0" {
-//
-//		// extract the status and attempt to find the error messageNode body
-//		status, _ := strconv.Atoi( statusNode.InnerText( ) )
-//
-//		messageNode := xmlquery.FindOne( doc, "//response/lst[@name='error']/str[@name='msg']")
-//		if messageNode != nil {
-//
-//			// if this is an error on a specific document, we can extract that information
-//			re := regexp.MustCompile(`\[(\d+),\d+\]`)
-//			match := re.FindStringSubmatch( messageNode.InnerText( ) )
-//			if match != nil {
-//				fmt.Printf( "%s", body )
-//				docnum, _ := strconv.Atoi( match[ 1 ] )
-//
-//				// return index of failing item
-//				return status, uint( docnum ) - 1, documentAddFailed
-//			}
-//		}
-//		return status, 0, fmt.Errorf( "%s", body )
-//	}
-//
-//	// all good
-//    return 0, 0, nil
-//}
 
 // examines the error and decides if if can be retried
 func (cl *cacheLoaderImpl) canRetry(err error) bool {
