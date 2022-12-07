b9b8870d44a36f9b6015dfade333377b11252957
Dave Goldstein <dpg3k@virginia.edu>
2022-12-07 07:23:19 -0500

    Check for ignore-cache attribute and behave appropriatly if it is located

--- cmd/virgo4-tracksys-enrich/tracksys-extract-step.go
+++ cmd/virgo4-tracksys-enrich/tracksys-extract-step.go
@@ -8,6 +8,11 @@ import (
 
 var errorNoIdentifier = fmt.Errorf("no identifier attribute located for document")
 
+// newly published items may appear here and in this case, the item identifier might not exist
+// in a service cache so this attribute is used to ignore the cache and lookup the item in an external
+// service anyway.
+var ignoreCacheAttributeName = "ignore-cache"
+
 // this is our actual implementation
 type tracksysExtractStepImpl struct {
 }
@@ -30,16 +35,29 @@ func (si *tracksysExtractStepImpl) Process(message *awssqs.Message, _ interface{
 	// extract the ID else we cannot do anything
 	id, foundId := message.GetAttribute(awssqs.AttributeKeyRecordId)
 	if foundId == true {
+
 		var err error
-		inTrackSys, err := TracksysIdCache.Contains(id)
-		if err != nil {
-			return false, nil, err
+		var lookupTrackSys = false
+
+		// see if we have the attribute telling us to ignore the cache
+		_, foundId = message.GetAttribute(ignoreCacheAttributeName)
+		if foundId == true {
+			log.Printf("INFO: id %s marked to IGNORE tracksys cache, getting details", id)
+			lookupTrackSys = true
+		} else {
+			// look the item up in the cache to see if tracksys knows about it
+			lookupTrackSys, err = TracksysIdCache.Contains(id)
+			if err != nil {
+				return false, nil, err
+			}
+			if lookupTrackSys == true {
+				log.Printf("INFO: located id %s in tracksys cache, getting details", id)
+			}
 		}
 
-		// tracksys contains information about this item
-		if inTrackSys == true {
-
-			log.Printf("INFO: located id %s in tracksys cache, getting details", id)
+		// tracksys (probably) contains information about this item
+		if lookupTrackSys == true {
+			// actually do the lookup work
 			trackSysDetails, err := TracksysIdCache.Lookup(id)
 			if err != nil {
 				return false, nil, err
